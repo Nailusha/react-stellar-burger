757c6411c115aff1ed595df5f9bb074ba65914aa
Nailya Yakimova <yakimovanailusha@yandex.ru>
2023-07-08 23:56:56 +0300

    commit1

---
+++ src/components/header/AppHeader/AppHeader.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+
+    render() {
+
+    return (
+
+        <div>
+            <a>Конструктор</a>
+            <a>Лента заказов</a>
+            <a></a>
+            <a>Личный кабинет</a>
+        </div>
+    
+    )}
+};
+
+export default Header
